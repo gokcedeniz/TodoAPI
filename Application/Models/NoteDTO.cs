2a4d9f80e362cfd31313967beacff97592efc7e2
gokce <gokce234342@ogr.duzce.edu.tr>
2025-08-01 14:39:17 +0300

    NoteDTO güncellendi

--- Application/Models/NoteDTO.cs
+++ Application/Models/NoteDTO.cs
@@ -6,8 +6,29 @@ using System.Threading.Tasks;
 
 namespace Application.Models
 {
-    public class NoteDTO
-    {
 
+    public class NoteListDTO
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+    }
+
+    public class NoteDetailDTO : NoteListDTO
+    {
+        public string Content { get; set; }
+    }
+
+    public class NoteInsertDTO
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Category { get; set; }
+    }
+
+    public class NoteUpdateDTO : NoteInsertDTO
+    {
+        public Guid Id { get; set; }
     }
 }
+
